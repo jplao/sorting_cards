2bc9a3a4e59c21a15c628206ab6948dfcd295af4
Jenn Lao <jenn.p.lao@gmail.com>
2018-08-18 07:06:43 -0600

    Add initial tests

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,7 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/bank'
 require './lib/card'
 
-class Card < Minitest::Test
-end 
+class CardTest < Minitest::Test
+
+  def test_check_it_exists
+    card = Card.new("Ace", "Spades")
+    assert_instance_of Card, card
+  end
+
+  def test_values_exist
+    card = Card.new("Ace", "Spades")
+    expected = "Ace"
+    actual = card.value
+    assert_equal expected, actual
+  end
+
+  def test_suits_exists
+    card = Card.new("Ace", "Spades")
+    expected = "Spades"
+    actual = card.suit
+    assert_equal expected, actual
+  end
+end
