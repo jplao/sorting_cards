dd76a1f9a38a58f77b65259b128eda0eb26a7790
Jenn Lao <jenn.p.lao@gmail.com>
2018-08-19 07:00:27 -0600

    Add storage test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -8,4 +8,13 @@ class CardTest < Minitest::Test
     deck = Deck.new(card_1)
     assert_instance_of Deck, card
   end
+
+  def test_it_can_get_cards
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    expected = [card_1, card_2, card_3]
+    actual = deck.cards
+  end
 end 
