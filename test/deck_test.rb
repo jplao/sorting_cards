b695f556c6b507dbf6cdc636a0dca40ad4dfe5a4
Jenn Lao <jenn.p.lao@gmail.com>
2018-08-19 07:03:26 -0600

    Add deck count test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,12 +9,22 @@ class CardTest < Minitest::Test
     assert_instance_of Deck, card
   end
 
-  def test_it_can_get_cards
+  def test_it_can_store_cards
     card_1 = Card.new("3","Hearts")
     card_2 = Card.new("4", "Clubs")
     card_3 = Card.new("5", "Diamonds")
     deck = Deck.new([card_1, card_2, card_3])
     expected = [card_1, card_2, card_3]
     actual = deck.cards
+    assert_equal expected, actual 
   end
-end 
+
+  def test_it_can_count_stored_cards
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    expected = 3
+    actual = deck.count
+    assert_equal expected, actual
+end
