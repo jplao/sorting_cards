02ad48de275a4eeaf56de105695f68a3e7cc1593
Jenn Lao <jenn.p.lao@gmail.com>
2018-08-21 04:19:23 -0600

    Update current card and number correct methods

--- lib/round.rb
+++ lib/round.rb
@@ -10,21 +10,28 @@ class Round
   def initialize(deck)
     @deck = deck
     @guesses = []
+    @deck_index = 0
     @correct_guesses = 0
+
   end
 
 
   def current_card
-    @deck.cards.first
+    @deck.cards[@deck_index]
   end
 
   def record_guess(guess)
     response = guess[:value] + " of " + guess[:suit]
     guess = Guess.new(response, current_card)
+    @deck_index += 1
     @guesses << guess
   end
 
-
+  def number_correct
+    if @guesses.first.feedback == "Correct!"
+      @correct_guesses += 1
+    end
+  end
 
 binding.pry
 end
