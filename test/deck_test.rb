e5be3f40e217a74f0d5b49ef664eea79e0b80ead
Jenn Lao <jenn.p.lao@gmail.com>
2018-08-21 08:56:26 -0600

    Add test methods for round and deck classes

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,8 +5,11 @@ require './lib/deck'
 class DeckTest < Minitest::Test
 
   def test_check_it_exists
-    deck = Deck.new(card_1)
-    assert_instance_of Deck, card
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_instance_of Deck, deck
   end
 
   def test_it_can_store_cards
@@ -27,4 +30,5 @@ class DeckTest < Minitest::Test
     expected = 3
     actual = deck.count
     assert_equal expected, actual
+  end
 end
