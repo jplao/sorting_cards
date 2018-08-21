dd86ebda448ae8a8a5770b188b78e73ab11b9946
Jenn Lao <jenn.p.lao@gmail.com>
2018-08-20 20:00:08 -0600

    Add record guess function

--- lib/round.rb
+++ lib/round.rb
@@ -1,18 +1,26 @@
 require 'pry'
 require './lib/deck'
+require './lib/card'
+require './lib/guess'
 
 class Round
-  attr_reader :deck
+  attr_reader :deck, :guesses
 
   def initialize(deck)
     @deck = deck
     @guesses = []
   end
 
-  def guesses
-    @guesses
-  end 
 
+  def current_card
+    @deck.cards.first
+  end
+
+  def record_guess(guess)
+    response = guess[:value] + " of " + guess[:suit]
+    guess = Guess.new(response, current_card)
+    @guesses << guess
+  end
 
 binding.pry
 end
